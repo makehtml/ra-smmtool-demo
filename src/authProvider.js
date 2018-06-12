ede51df87bbbe522820a846ae9caf9446a489efc
snpr <vaugk13@gmail.com>
2018-06-12 15:49:57 +0500

    пытаюсь настроить авторизацию

--- src/authProvider.js
+++ src/authProvider.js
@@ -1,32 +1,41 @@
-import { AUTH_LOGIN, AUTH_LOGOUT, AUTH_ERROR, AUTH_CHECK } from 'react-admin';
+import { AUTH_LOGIN, AUTH_LOGOUT, AUTH_ERROR, AUTH_GET_PERMISSIONS, AUTH_CHECK } from 'react-admin';
+import decodeJwt from 'jwt-decode';
 
 export default (type, params) => {
-    // called when the user attempts to log in
     if (type === AUTH_LOGIN) {
-        const { username } = params;
-        localStorage.setItem('username', username);
-        // accept all username/password combinations
-        return Promise.resolve();
+        const { username, password } = params;
+        const request = new Request('http://dev.smmtool.ru:4444/login', {
+            method: 'POST',
+            body: JSON.stringify({ username, password }),
+            headers: new Headers({ 'Content-Type': 'application/json' }),
+        })
+        return fetch(request)
+            .then(response => {
+                if (response.status < 200 || response.status >= 300) {
+                    throw new Error(response.statusText);
+                }
+                return response.json();
+            })
+            .then(({ token }) => {
+                const decodedToken = decodeJwt(token);
+                localStorage.setItem('token', token);
+                localStorage.setItem('role', decodedToken.role);
+            });
     }
-    // called when the user clicks on the logout button
     if (type === AUTH_LOGOUT) {
-        localStorage.removeItem('username');
+        localStorage.removeItem('token');
+        localStorage.removeItem('role');
         return Promise.resolve();
     }
-    // called when the API returns an error
     if (type === AUTH_ERROR) {
-        const { status } = params;
-        if (status === 401 || status === 403) {
-            localStorage.removeItem('username');
-            return Promise.reject();
-        }
-        return Promise.resolve();
+        // ...
     }
-    // called when the user navigates to a new location
     if (type === AUTH_CHECK) {
-        return localStorage.getItem('username')
-            ? Promise.resolve()
-            : Promise.reject();
+        return localStorage.getItem('token') ? Promise.resolve() : Promise.reject();
     }
-    return Promise.reject('Unknown method');
+    if (type === AUTH_GET_PERMISSIONS) {
+        const role = localStorage.getItem('role');
+        return role ? Promise.resolve(role) : Promise.reject();
+    }
+    return Promise.reject('Unkown method');
 };
